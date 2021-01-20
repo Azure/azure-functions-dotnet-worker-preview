77db60de699e7b2f3e872c75c89b2b498d146659
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2021-01-20 11:57:30 -0800

    Adding basic middleware sample

---
+++ FunctionApp/Middleware/ApplicationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FunctionApp.Middleware
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IFunctionsWorkerApplicationBuilder UseSampleMiddleware(this IFunctionsWorkerApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton<SampleMiddleware>();
+
+            builder.Use(next =>
+            {
+                return context =>
+                {
+                    var middleware = context.InstanceServices.GetRequiredService<SampleMiddleware>();
+
+                    return middleware.Invoke(context, next);
+                };
+            });
+
+            return builder;
+        }
+    }
+}
