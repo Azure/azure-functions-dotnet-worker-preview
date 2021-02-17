d93508c50f66111ff2064acc11a811f94b6ce19a
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2021-02-16 17:56:10 -0800

    Updating package references to preview4 and updating HTTP sample

--- FunctionApp/Middleware/ApplicationBuilderExtensions.cs
+++ FunctionApp/Middleware/ApplicationBuilderExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
